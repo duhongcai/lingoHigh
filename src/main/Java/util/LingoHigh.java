ee81915c875f25d831e32fe9f6501009c894d201
duhongcaisb@163.com <11032320>
2016-09-07 16:47:00 +0800

    你好 李狗嗨

---
+++ src/main/Java/util/LingoHigh.java
@@ -0,0 +1,10 @@
+package util;
+
+/**
+ * Created by DuHongcai on 2016/9/7.
+ */
+public class LingoHigh {
+    public static void main(String[] args) {
+        System.out.print("你好 李狗嗨");
+    }
+}

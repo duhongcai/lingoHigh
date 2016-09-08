f8915da593869bbd45b7f8d3cea59c4c104fb84f
duhongcaisb@163.com <11032320>
2016-09-08 17:41:20 +0800

    lingoHigh

--- src/main/Java/lingoHigh/controller/LoginController.java
+++ src/main/Java/lingoHigh/controller/LoginController.java
@@ -10,10 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/")
 public class LoginController {
 
-    @RequestMapping(value = "/")
-    public String toIndex(){
-        return "index";
-    }
 }
 
 

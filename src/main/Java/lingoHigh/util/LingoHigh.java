d649c64e3a921d48adcc47dfdf993fec548b5d60
duhongcaisb@163.com <11032320>
2016-09-10 09:56:19 +0800

    日志管理

--- src/main/Java/lingoHigh/util/LingoHigh.java
+++ src/main/Java/lingoHigh/util/LingoHigh.java
@@ -1,10 +1,15 @@
 package lingoHigh.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by DuHongcai on 2016/9/7.
  */
 public class LingoHigh {
+    private static Logger logger = LoggerFactory.getLogger(LingoHigh.class);
     public static void main(String[] args) {
+        logger.info("你好 LingoHigh");
         System.out.print("你好 李狗嗨");
     }
 }

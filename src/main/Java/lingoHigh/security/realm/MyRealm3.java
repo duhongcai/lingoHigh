0fdcd8de6f611d8675b727e6cc7eb81095558507
duhongcaisb@163.com <11032320>
2016-09-19 20:13:50 +0800

    权限管理基础配置

---
+++ src/main/Java/lingoHigh/security/realm/MyRealm3.java
@@ -0,0 +1,29 @@
+package lingoHigh.security.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by DuHongcai on 2016/9/19.
+ */
+public class MyRealm3 implements Realm {
+    public String getName() {
+        return "MyRealm3";
+    }
+
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof UsernamePasswordToken;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String) token.getPrincipal();
+        String password = new String((char[]) token.getCredentials());
+        if (!"zhang".equals(username)){
+            throw new UnknownAccountException();
+        }
+        if (!"123".equals(password)){
+            throw new IncorrectCredentialsException();
+        }
+        return new SimpleAuthenticationInfo(username+"@163.com",password,getName());
+    }
+}
---
+++ src/main/Java/lingoHigh/security/testShiroWorld.java
@@ -0,0 +1,91 @@
+package lingoHigh.security;
+
+import lingoHigh.util.exception.TestLingoHighException;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.springframework.util.Assert;
+
+/**
+ * Created by DuHongcai on 2016/9/19.
+ */
+public class TestShiroWorld {
+
+    private void shiroDemo1(){
+        //利用ini配置文件构造SecurityManager工厂
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:security/shiro.ini");
+        //创建SecurityManager对象
+        SecurityManager securityManager = factory.getInstance();
+        //将SecurityManager赋给SecurityUtils
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //获取当前访客Subject
+        Subject subject = SecurityUtils.getSubject();
+
+        //模拟接收到用户名密码 并生成token令牌
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang","1234");
+        try{
+            subject.login(token);
+        }catch (AuthenticationException erroInfo){
+            erroInfo.printStackTrace();
+        }
+       if (subject.isAuthenticated()){
+           System.out.println("已登录");
+       }
+        subject.logout();
+        if (!subject.isAuthenticated()){
+            System.out.println("已退出或未登录");
+        }
+    }
+
+    //自定义realm
+    private void testCustomRealm(){
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:security/shiro-realm.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhangsan","123");
+        try{
+            subject.login(token);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        if (subject.isAuthenticated()){
+            System.out.println("已登录");
+        }
+    }
+
+    //自定义多个realm ???????? 为什么第二个不行呢？？？？
+    private void testMuliRealm(){
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:security/shiro-realm.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("wang2","123");
+        subject.login(token);
+        System.out.println(subject.getPrincipal());
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        TestShiroWorld shiroWorld = new TestShiroWorld();
+        //shiroWorld.shiroDemo1();
+        //shiroWorld.testCustomRealm();
+        shiroWorld.testMuliRealm();
+        tearDown();
+    }
+
+    private static void tearDown(){
+        ThreadContext.unbindSubject();
+    }
+}
